ab44419e8a005e0101dd5970c3b2b3f0375af16e
Yura <yura@Yuras-MacBook-Air.local>
2018-10-09 20:41:01 +0300

    create paje

---
+++ js/index.js
@@ -0,0 +1,23 @@
+// create variable
+let logoMenu = document.getElementById('menu');
+let nav = document.getElementById('nav');
+let img = document.getElementById('slider');
+let imgSlider = 1;
+// end create variable
+
+
+// Выпадающее menu
+logoMenu.addEventListener('click',function () {
+    nav.classList.toggle("toogle");
+});
+// end nav menu
+
+
+/*Start Slick*/
+$(document).ready(function(){
+    $('.slider').slick({
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+  });
+/*End Slick*/
