12f6b133f9046bae6082350dc8debfa996abbed0
Yura <yura@Yuras-MacBook-Air.local>
2018-10-10 11:56:40 +0300

    add scroll

--- js/index.js
+++ js/index.js
@@ -17,7 +17,28 @@ logoMenu.addEventListener('click',function () {
 $(document).ready(function(){
     $('.slider').slick({
         autoplay: true,
-        autoplaySpeed: 15000,
+        autoplaySpeed: 5000,
     });
   });
 /*End Slick*/
+
+// Smooth scroll
+// (function ($) {
+//     $(".test").click(function () {
+//       $("html, body").animate({
+//         scrollTop: $($(this).attr("#our-benefits")).offset().top
+//       },2000);
+//       return false;
+//     });
+//   })($);
+
+$(document).ready(function(){
+    $('.test, .test2').click( function(){ // ловим клик по ссылке с классом go_to
+	var scroll_el = $(this).attr('href'); // возьмем содержимое атрибута href, должен быть селектором, т.е. например начинаться с # или .
+        console.log(scroll_el);
+    if ($(scroll_el).length != 0) { // проверим существование элемента чтобы избежать ошибки
+	    $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 500); // анимируем скроолинг к элементу scroll_el
+        }
+	    return false; // выключаем стандартное действие
+    });
+});
